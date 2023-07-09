c3f95d9a37a7211f48b6139b093a107381d92fe4
Maymunah Waqas Butt <maymunahwaqas@outlook.com>
2023-07-10 00:30:23 +0100

    Initialised Temperature and CurrentlyRaining variables

--- src/WeatherCheck.java
+++ src/WeatherCheck.java
@@ -1,5 +1,19 @@
+import java.time.Year;
+
 public class WeatherCheck {
     public static void main (String[] arg){
+int Temperature = 10;
+                boolean CurrentlyRaining = Boolean.parseBoolean("Yes");
+
+
+
+
+
+
+
+
+
+
 
     }
 }
